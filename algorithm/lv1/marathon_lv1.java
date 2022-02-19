0cfe5b5067905dc9cb225626c9725728cc89cf89
dongjji <y2kdj9723@naver.com>
2022-02-19 17:15:35 +0900

    Day263 프로그래머스 완주하지 못한 선수 lv1

---
+++ algorithm/lv1/marathon_lv1.java
@@ -0,0 +1,22 @@
+package lv1;
+
+import java.util.*;
+
+class marathon_lv1 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> athletes = new HashMap<>();
+        for (String p: participant) {
+            athletes.put(p, athletes.getOrDefault(p, 0) + 1);
+        }
+        for (String p: completion) {
+            athletes.put(p, athletes.get(p)-1);
+        }
+        for (String p: athletes.keySet()) {
+            if (athletes.get(p) == 1) {
+                return p;
+            }
+        }
+        return answer;
+    }
+}
