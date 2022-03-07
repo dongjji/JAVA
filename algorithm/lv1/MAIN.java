195e9390e0a2c413a34dd13c4a057b44a1a0cfe5
dongjji <y2kdj9723@naver.com>
2022-03-07 19:57:20 +0900

    Day279 array sort by custom option

--- algorithm/lv1/MAIN.java
+++ algorithm/lv1/MAIN.java
@@ -4,5 +4,20 @@
 
 class Solution {
   public static void main(String[] args) {
+    Integer[] arr = {1, 2, 3, 4, 5};
+    HashMap<Integer, Double> hm = new HashMap<Integer, Double>();
+    hm.put(1, 0.3);
+    hm.put(2, 0.1);
+    hm.put(3, 0.4);
+    hm.put(4, 0.9);
+    hm.put(5, 0.8);
+
+    Arrays.sort(arr, new Comparator<Integer>() {
+      public int compare(Integer idx1, Integer idx2) {
+          return Double.compare(hm.get(idx1), hm.get(idx2));
+      }
+    });
+
+    System.out.println(Arrays.toString(arr));
   }
 }
--- algorithm/readme.md
+++ algorithm/readme.md
@@ -3,3 +3,4 @@
 - [List<Integer> <=> int[]](https://blog.naver.com/y2kdj9723/222653507880, "개발할 생각")
 - [int[], int array에서 max값 도출하기](https://blog.naver.com/y2kdj9723/222653513442, "개발할 생각")
 - [Array sort, reverse_sort](https://blog.naver.com/y2kdj9723/222660097668, "개발할 생각")
+- [Arrays sort by custom option](https://blog.naver.com/y2kdj9723/222666283896, "개발할 생각")
